793d99a7b442ac8f767c2e5d76ae23a9f53338b8
Konsta <n0jako00@students.oamk.fi>
2021-10-07 23:44:03 +0300

    API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react'
+const API_URL = 'http://numbersapi.com/';
+
+/* API_KEY ei tarvita tämän APIn kanssa. */
 
 function App() {
+
+  const [number, setNumber] = useState(0);
+  const [result, setResult] = useState("");
+
+  async function search(e) {
+    e.preventDefault();
+    try {
+      const address = API_URL + number + '/?json';
+      const response = await fetch(address);
+
+      if (response.ok) {
+        const json = await response.json();
+        setResult(json.text);
+      } else {
+        alert('Error finding number.');
+        console.log(response);
+      }
+    } catch (err) {
+      alert(err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <form onSubmit={search}>
+        <div>
+          <label>Type a random number&nbsp;</label>
+          <input type="number" value={number} onChange={e => setNumber(e.target.value)} ></input>
+        </div>
+        <div>
+          <output>{result}</output>
+        </div>
+        <div>
+          <button>Search</button>
+        </div>
+      </form>
     </div>
   );
 }
